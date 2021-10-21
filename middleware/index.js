fc4ad34f214ab4f5c2c832a359467c9e86aa624d
TheNhatAT <nhat0103st@gmail.com>
2021-10-21 23:17:48 +0700

    nhat: fix name of template.model file

--- middleware/index.js
+++ middleware/index.js
@@ -4,6 +4,5 @@
  * 1.Kiểm tra người dùng đã xác thực
  * 2.Kiểm tra xem JWT của người dùng có hợp lệ hay không?
  * 3.Kiểm tra xem người dùng có role hợp lệ hay không?
- * 4.Kiểm tra người dùng có được phép truy cập vào link hay không?
  * ****************************************
  */
