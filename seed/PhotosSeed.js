ff9a64e2022931507c4551cadee89a443d969e9d
Phạm Công Minh <minhctthvn@gmail.com>
2021-11-12 16:09:36 +0700

    minh: chỉnh lại tên url

--- seed/PhotosSeed.js
+++ seed/PhotosSeed.js
@@ -13,35 +13,35 @@ exports.PhotosSeed = async function () {
     // Seed dữ liệu
     await Photos(dbConnect()).create([
         {
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\1001LakesideVillas.jpg"
+            url: "../upload/homestays_photos/1001LakesideVillas.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\AnVilla15.jpg"
+            url: "../upload/homestays_photos/AnVilla15.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\AnVuiCottage19.jpg"
+            url: "../upload/homestays_photos/AnVuiCottage19.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\BAVIPadmeHome.jpg"
+            url: "../upload/homestays_photos/BAVIPadmeHome.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\BietthuhoahongBT4.jpg"
+            url: "../upload/homestays_photos/BietthuhoahongBT4.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\ChoaiVillaSocSon.jpg"
+            url: "../upload/homestays_photos/ChoaiVillaSocSon.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\EmbossiGarden.jpg"
+            url: "../upload/homestays_photos/EmbossiGarden.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\MARSNNM.jpg"
+            url: "../upload/homestays_photos/MARSNNM.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\RoseVilla.jpg"
+            url: "../upload/homestays_photos/RoseVilla.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\SocSonRiverside.jpg"
+            url: "../upload/homestays_photos/SocSonRiverside.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\StormVilla.jpg"
+            url: "../upload/homestays_photos/StormVilla.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\StreamHouse.jpg"
+            url: "../upload/homestays_photos/StreamHouse.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\TheMoonlight.jpg"
+            url: "../upload/homestays_photos/TheMoonlight.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\Villas.jpg"
+            url: "../upload/homestays_photos/Villas.jpg"
         },{
-            url: "C:\\Users\\Hi\\WebstormProjects\\nmcnpm-be\\upload\\homestays_photos\\XanhVilla04.jpg"
+            url: "../upload/homestays_photos/XanhVilla04.jpg"
         },
     ]);
     console.log('seeded user OK!');
