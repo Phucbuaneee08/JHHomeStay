be07cbe6ee7dfb43082883b0c6784609bd80d3f6
PhamCongMinh <minhctthvn@gmail.com>
2021-12-29 18:37:50 +0000

    Hotfix/fix api create homestay api update homestay

--- modules/super-admin/homestays/super-admin.homestays.controller.js
+++ modules/super-admin/homestays/super-admin.homestays.controller.js
@@ -8,14 +8,32 @@ exports.createInformationForHomestay = async (req, res) => {
 
         const adminId = data.adminId ? data.adminId : null;
         const homestayName = data.name ? data.name : " ";
+        const homestayPrice = data.price ? data.price : 0;
         const homestayType = data.type ? data.type : " ";
+        const homestayAddress = data.address ? data.address : " ";
         const homestayProvince = data.province ? data.province : " ";
         const homestayDistrict = data.district ? data.district : " ";
-        const homestayAddress = data.address ? data.address : " ";
-        const homestayPrice = data.price ? data.price : 0;
+        const homestayLatitude = data.latitude ? data.latitude : " ";
+        const homestayLongitude = data.longitude ? data.longitude : " ";
+        const homestayArea = data.area ? data.area : 0;
+        const homestayDescription = data.description ? data.description : " ";
+        const homestayAvailable = data.available ? data.available : 0;
+        let homestayServices, homestayGeneralServices, homestayAmenities, homestayPhotos;
+        if (data.services == '' || data.services == null) {
+            homestayServices = null
+        } else homestayServices = data.services;
+        if (data.generalServices == '' || data.generalServices == null) {
+            homestayGeneralServices = null
+        } else homestayGeneralServices = data.generalServices;
+        if (data.amenities == '' || data.amenities == null) {
+            homestayAmenities = null
+        } else homestayAmenities = data.amenities;
+        if (data.photos == '' || data.photos == null) {
+            homestayPhotos = null
+        } else homestayPhotos = data.photos;
 
         //Tạo homestay 
-        const homestay = await HomestayService.createHomestay(adminId, homestayName, homestayProvince, homestayDistrict, homestayAddress, homestayType, homestayPrice );
+        const homestay = await HomestayService.createHomestay(adminId, homestayName, homestayProvince, homestayDistrict, homestayAddress, homestayType, homestayPrice, homestayLatitude, homestayLongitude, homestayArea, homestayDescription, homestayAvailable, homestayServices, homestayGeneralServices, homestayAmenities, homestayPhotos );
 
         return res.status(200).json({
             success: true,
--- modules/user/homestays/user.homestays.service.js
+++ modules/user/homestays/user.homestays.service.js
@@ -149,3 +149,77 @@ exports.getCheckInAndOutDateByIdHomestay = async (id) => {
     ]);
     return bill;
 }
+
+//Update Homestays Service
+exports.updateHomestay = async (homestayId, homestayName, homestayPrice, homestayType, homestayAddress,homestayProvince, homestayDistrict, homestayLatitude, homestayLongitude, homestayArea, homestayDescription, homestayAvailable, homestayAmenities, homestayServices, homestayGeneralServices, homestayPhotos) =>{
+    // Tạo object rỗng để chứa các thông tin cần cập nhật
+    let setHomestay = {};
+
+    // Kiểm tra và cập nhật
+    if( homestayName ){
+        setHomestay = {...setHomestay, "name": homestayName};
+    }
+
+    if( homestayPrice ){
+        setHomestay = {...setHomestay, "price": homestayPrice};
+    }
+
+    if( homestayType ){
+        setHomestay = {...setHomestay, "type": homestayType};
+    }
+
+    if( homestayAddress ){
+        setHomestay = {...setHomestay, "address": homestayAddress};
+    }
+
+    if( homestayProvince ){
+        setHomestay = {...setHomestay, "province": homestayProvince};
+    }
+
+    if( homestayDistrict ){
+        setHomestay = {...setHomestay, "district": homestayDistrict};
+    }
+
+    if( homestayLatitude ){
+        setHomestay = {...setHomestay, "latitude": homestayLatitude};
+    }
+
+    if( homestayLongitude ){
+        setHomestay = {...setHomestay, "longitude": homestayLongitude};
+    }
+
+    if( homestayArea ){
+        setHomestay = {...setHomestay, "area": homestayArea};
+    }
+
+    if( homestayDescription ){
+        setHomestay = {...setHomestay, "description": homestayDescription};
+    }
+
+    if( homestayAvailable ){
+        setHomestay = {...setHomestay, "available": homestayAvailable};
+    }
+
+    if( homestayAmenities ){
+        setHomestay = {...setHomestay, "amenities": homestayAmenities};
+    }
+
+    if( homestayServices ){
+        setHomestay = {...setHomestay, "services": homestayServices};
+    }
+
+    if( homestayGeneralServices ){
+        setHomestay = {...setHomestay, "generalServices": homestayGeneralServices};
+    }
+
+    if( homestayPhotos ){
+        setHomestay = {...setHomestay, "photos": homestayPhotos};
+    }
+
+    // Cập nhật vào database
+    await Homestays(db).deleteOne({ _id: homestayId })
+    const homestay = await Homestays(db).create(setHomestay);
+
+    return homestay;
+}
+
