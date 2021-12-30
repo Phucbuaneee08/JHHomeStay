b332ce7b4f407037cdd2c4dfd3f48b468b01f054
Phạm Việt Hoàng <hoang12b1ksa@gmail.com>
2021-12-30 22:06:25 +0700

    hoang: update API revenue statistics

--- seed/HomestaysSeed-2.js
+++ seed/HomestaysSeed-2.js
@@ -15,7 +15,7 @@ let dbConnect = () => {
         }
     // Cơ sở dữ liệu cục bộ
     let db =  mongoose.createConnection('mongodb://localhost:27017/JadeHillHomestays', connectOptions);
-    // let db =  mongoose.createConnection('mongodb+srv://jadehillhomestays:1234@cluster0.nwvtu.mongodb.net/jadehillhomestays?retryWrites=true&w=majority', connectOptions);
+    //let db =  mongoose.createConnection('mongodb+srv://jadehillhomestays:1234@cluster0.nwvtu.mongodb.net/jadehillhomestays?retryWrites=true&w=majority', connectOptions);
 
     return db
 }
